a894ec5ef75ca5eea0e94923b36b361ed14c3a18
wabisabilife <120066256+wabisabilife@users.noreply.github.com>
2022-12-28 17:36:24 +0100

    Poceo nav
    malo nav radio i namjestio one kartice da mi fino staggeruju

--- javascripts/indexAnimations.js
+++ javascripts/indexAnimations.js
@@ -1,3 +1,6 @@
+
+// prikazivanje side navbara
+
 let side_icon = document.getElementById("side_icon");
 let side_nav = document.getElementById("side_nav");
 let side_x = document.getElementById("side_x");
@@ -10,4 +13,37 @@ side_icon.addEventListener("click", ()=>{
 
 side_x.addEventListener("click", ()=>{
     side_nav.style.marginRight = "-30vw";
+})
+
+//pillupam landing header i pokazem paragrafe
+let landing_header = document.getElementById("landing_header")
+let landing_paragraphs = document.getElementById("landing_paragraphs")
+
+setTimeout(()=>{
+    landing_header.classList.add("pullup")
+    landing_paragraphs.classList.add("show")
+}, 1000)
+
+
+
+//animacija dijela sa strelicom
+
+let header_1 = document.getElementById("naslov_1");
+let header_2 = document.getElementById("naslov_2");
+let header_3 = document.getElementById("naslov_3");
+let header_4 = document.getElementById("naslov_4");
+let header_5 = document.getElementById("naslov_4");
+
+let paragraf_1 = document.getElementById("tekst_1");
+let paragraf_2 = document.getElementById("tekst_2");
+let paragraf_3 = document.getElementById("tekst_3");
+let paragraf_4 = document.getElementById("tekst_4");
+let paragraf_5 = document.getElementById("tekst_5");
+
+let wrapper_strelica = document.getElementById("strelica_section");
+let main = document.getElementById("main")
+
+document.addEventListener("scroll",()=>{
+    console.log(window.scrollY, "  ", main.scrollHeight - screen.height)
+    if(wrapper_strelica.offsetTop <= window.scrollY) console.log("radi");
 })
